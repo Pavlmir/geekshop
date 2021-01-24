523648393402214a960020ab94ec01190a54eeae
pavlmir <mirprost@gmail.com>
2021-01-24 18:30:40 +0300

    make lesson-2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+import json
+import os
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'Магазин'}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    module_dir = os.path.dirname(__file__)
+    with open(os.path.join(module_dir, 'fixtures/products.json'), "r", encoding='UTF-8') as f:
+        to_python = json.loads(f.read())
+
+    context = {'title': 'Товары',
+               'products': to_python}
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {'title': 'Контакты'}
+    return render(request, 'mainapp/contact.html', context)
