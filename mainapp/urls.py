20272c01a37a970d614ea2609be0a929c9cec722
pavlmir <mirprost@gmail.com>
2021-02-16 18:40:22 +0300

    lesson-8

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-import mainapp.views as mainapp
+from mainapp.views import products
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('', products, name='index'),
+    path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page')
 ]
